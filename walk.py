0591027d7b21227ff5a10b13d521ad8b676cacd6
HighW4y2H3ll <huzh@nyu.edu>
2019-12-07 19:48:57 +0000

    load binwalk from local

--- walk.py
+++ walk.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
 import sys, os
-CUR_DIR = os.path.realpath(os.path.dirname(__file__))
-print(CUR_DIR)
-sys.path.append(CUR_DIR)
+import importlib
 
-import binwalk
+CUR_DIR = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'binwalk/src/binwalk/__init__.py')
 
-with binwalk.Modules(*(['-q', '-f', 'log'] + sys.argv[1:]), signature=True) as mod:
+#import binwalk
+_binwalk_spec = importlib.util.spec_from_file_location('binwalk', CUR_DIR)
+binwalk = importlib.util.module_from_spec(_binwalk_spec)
+sys.modules[_binwalk_spec.name] = binwalk
+_binwalk_spec.loader.exec_module(binwalk)
+
+
+LOGGING = "log"
+
+with binwalk.Modules(*sys.argv[1:], signature=True, quiet=True, log=LOGGING) as mod:
     print(mod.execute())
