dc9b2f06f72c63fc127a34fc77dcb3ddd06723c1
HighW4y2H3ll <huzh@nyu.edu>
2019-12-08 17:12:51 +0000

    more refactoring, so we could keep track of states across results

--- walk.py
+++ walk.py
@@ -17,36 +17,57 @@ LOGGING = "log"
 
 
 class CALLBACK(object):
-    def init(self, binfile, off, size):
-        print(binfile)
-        print(off)
-        print(size)
+    def __init__(self, binfile):
+        self.binfile = binfile
+        self.init() # Initialize method from derived class
+
+    def update(self, off, size):
+        pass
+
+    def init(self):
+        pass
+
 
 class LZMA_CB(CALLBACK):
-    pass
+    def init(self):
+        pass
+
+    def update(self, off, size):
+        print(self.binfile)
+        print(off)
+        print(size)
+        binwalk.core.common.BlockFile(self.binfile)
 
 class SQUASHFS_CB(CALLBACK):
     pass
 
 
-def dispatch_callback(desc):
-    if desc.lower().startswith("lzma compressed data"):
-        return LZMA_CB()
-    elif desc.lower().startswith("squashfs filesystem"):
-        return SQUASHFS_CB()
+
+class Extractor(object):
+    def __init__(self, binfile):
+        self.binfiles = [binfile]
+        self.lzma_cb = LZMA_CB(binfile)
+        self.squashfs_cb = SQUASHFS_CB(binfile)
+
+    def dispatch_callback(self, desc):
+        if desc.lower().startswith("lzma compressed data"):
+            return self.lzma_cb
+        elif desc.lower().startswith("squashfs filesystem"):
+            return self.squashfs_cb
 
 
-def extract(binfiles, workdir):
-    with binwalk.Modules(*binfiles, signature=True, quiet=True, log=os.path.join(workdir, LOGGING)) as mod:
-        executed_mods = mod.execute()
-        assert(len(executed_mods) == 1)
-        sigmod = executed_mods[0]
-        assert(isinstance(sigmod, binwalk.modules.Signature))
+    def extract(self, workdir):
+        with binwalk.Modules(*self.binfiles, signature=True, quiet=True, log=os.path.join(workdir, LOGGING)) as mod:
+            executed_mods = mod.execute()
+            assert(len(executed_mods) == 1)
+            sigmod = executed_mods[0]
+            assert(isinstance(sigmod, binwalk.modules.Signature))
 
-        for result in sigmod.results:
-            if result.valid:
-                cb = dispatch_callback(result.description)
-                cb.init(result.file.path, result.offset, result.size)
+            for result in sigmod.results:
+                if result.valid:
+                    cb = self.dispatch_callback(result.description)
+                    cb.update(result.offset, result.size)
 
 if __name__ == "__main__":
-    extract(sys.argv[1:], ".")
+    for fn in sys.argv[1:]:
+        Extractor(fn).extract(".")
